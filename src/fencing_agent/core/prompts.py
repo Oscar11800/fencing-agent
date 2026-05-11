60c207dfbf77fe258b8e1ab356549a4f82caa290
Oscar Chen <oscar1149412@gmail.com>
2026-05-11 14:33:22 -0500

    OAI API

---
+++ src/fencing_agent/core/prompts.py
@@ -0,0 +1,4 @@
+SYSTEM_PROMPT = "You are a helpful assistant for the UChicago Fencing Club. \
+Answer questions about practices, competitions, membership, and donations. \
+Be friendly and professional. \
+If you don't know the answer, direct the user to email uofcfencingprez@gmail.com."
---
+++ src/fencing_agent/services/agent.py
@@ -0,0 +1,26 @@
+import uuid 
+from sqlalchemy.ext.asyncio import AsyncSession
+from fencing_agent.db.repositories import get_history, save_message
+from fencing_agent.clients.openai import get_chat_response
+from fencing_agent.core.prompts import SYSTEM_PROMPT
+
+# TODO: Add limit to history being sent, history context compactor after reaching a certain limit
+async def handle_message( db: AsyncSession, session_id: uuid.UUID, user_message: str):
+    history = await get_history(db, session_id)
+    # build the prompt
+    # start with the system prompt
+    messages = [{"role": "system", "content": SYSTEM_PROMPT}]
+    
+    # supply history
+    for msg in history:
+        messages.append({"role": msg.role, "content": msg.content})
+    
+    # current user msg
+    messages.append({"role": "user", "content": user_message})
+    
+    # call OAI API
+    await response = get_chat_response(messages)
+
+    await save_message(db, session_id, "user", user_message)
+    await save_message(db, session_id, "assistant", response)
+    return response
